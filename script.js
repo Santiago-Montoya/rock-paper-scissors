4e101bc383ac0f1008dcf2e80b9ae0f862e71048
Santiago Montoya <smpalacio2013@gmail.com>
2023-05-31 22:32:33 -0500

    Working Version

--- script.js
+++ script.js
@@ -1,52 +1,82 @@
+let playerScore = 0;
+let computerScore = 0;
+const computer = computerSelection();
 
-function getPlayerSelection() {
-    const randomChoice = Math.floor(Math.random() * 100) +1;
-    if (randomChoice <= 33.33) {
-        return "rock";
-    } else if (randomChoice > 33.33 && randomChoice < 66.66) {
-        return "paper";
-    } else {
-        return "scissors"
+
+function computerSelection() {
+    const randomChoice = Math.floor(Math.random() * 3);
+    switch (randomChoice) {
+        case 0: 
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors"
+        default:
+            break;
     }
 }
 
-console.log(`player: ${getPlayerSelection()}`);
+// function playerSelection() {
+//     const randomChoice = Math.floor(Math.random() * 3);
+//     switch (randomChoice) {
+//         case 0: 
+//             return "rock";
+//             break;
+//         case 1:
+//             return "paper";
+//             break;
+//         case 2:
+//             return "scissors"
+//         default:
+//             break;
+//     }
+// }
 
-function getComputerSelection() {
-    const randomChoice = Math.floor(Math.random() * 100) +1;
-    if (randomChoice <= 33.33) {
-        return "rock";
-    } else if (randomChoice > 33.33 && randomChoice < 66.66) {
-        return "paper";
-    } else {
-        return "scissors"
-    }
-}
 
-console.log(`computer: ${getComputerSelection()}`);
-const player = getPlayerSelection();
-const comp = getComputerSelection();
 
-function gameRound(player, comp) {
+
+function gameRound(player, computer) {
     
-    if (player === "rock"   && comp === "rock" || 
-    player === "paper"      && comp === "paper" ||
-    player === "scissors"   && comp === "scissors") {
-        return `It's a draw!`;
-    } else if ( player === "rock"       && comp === "paper" ||
-    player === "paper"      && comp === "scissors" ||
-    player === "scissors"   && comp === "rock") {
-        return `You lose! ${comp} beats ${player}.`;
+    if (computer === player) {
+        console.log(`DRAW! Computer ${computer} - Player: ${player}`);
+    } else if ( computer === "rock"     && player === "scissors"    ||
+                computer === "paper"    && player === "rock"        ||
+                computer === "scissors" && player === "paper") {
+        console.log(`COMPUTER WINS! Computer: ${computer} - Player: ${player}`);
+        computerScore++;
+        
+    } else if ( computer === "rock"     && player === "paper"       ||
+                computer === "paper"    && player === "scissors"    ||
+                computer === "scissors" && player === "rock") {
+        console.log(`YOU WIN! Player: ${player} - Computer: ${computer}`);
+        playerScore++;
     } else {
-        return `You win! ${player} beats ${comp}.`;
+        return "Invalid Input."
     }
 }
 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const player = window.prompt("Type: Rock, Paper, or Scissors");
+        const computer = computerSelection();
+        gameRound(player.toLowerCase(), computer);
+    }
+    
+    if (playerScore === computerScore) {
+        console.log("THE GAME IS A DRAW");
+    } else if (playerScore > computerScore) {
+        console.log("YOU WIN THE GAME");
+    } else {
+        console.log("THE COMPUTER WINS THE GAME");
+    }
+}
 
-console.log(`player: ${player}`);
-console.log(`computer: ${comp}`);
-
-console.log(gameRound(player, comp));
+game();
+console.log(`My score: ${playerScore}`);
+console.log(`Computer score: ${computerScore}`);
 
 
 
